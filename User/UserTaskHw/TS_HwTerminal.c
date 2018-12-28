321a29449d975aac564072536a0310e3c60939c9
FILIP\Филипп <filippopov32@gmail.com>
2018-12-28 17:24:51 +0300

    HwAPI. HwTerminal. Fix error with queue handle names

--- User/UserTaskHw/TS_HwTerminal.c
+++ User/UserTaskHw/TS_HwTerminal.c
@@ -20,24 +20,25 @@
 /* Include my libraries here */
 #include "defines.h"
 #include "tm_stm32f4_rtc.h"
-#include "tm_stm32f4_delay.h"
 #include "tm_stm32f4_usart.h"
 #include "tm_stm32f4_disco.h"
-#include "tm_stm32f4_fatfs.h"
+
+#include "HwAPI.h"
+#include "TS_HwQueue.h"
+#include "TS_task.h"
+
 
 static char tempString[500] = {""}; 
 //char* tempString2[20] = {""};
     
-void vTask_Terminal( void *pvParameters )
+void vTask_HwTerminal( void *pvParameters )
 {
-
-    extern QueueHandle_t xQueue_Terminal;
 	/* Initialize USART, TX: PB10, RX: PB11 */
     TM_USART_Init(USART2, TM_USART_PinsPack_2, (uint32_t)(115200.0 * 3.125)); // Why coefficient 3.125?
 
 	while( 1 )
 	{
-        xQueueReceive( xQueue_Terminal, &tempString, portMAX_DELAY );
+        xQueueReceive( xQueue_HwTerminal_Rx, &tempString, portMAX_DELAY );
 
        /* Send to USART */
         TM_USART_Puts( USART2, tempString );
