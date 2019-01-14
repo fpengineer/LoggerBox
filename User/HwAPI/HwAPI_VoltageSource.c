4fa33fe0ae5e3a6a7c48c72b1bc4865a3c40ae85
FILIP\Филипп <filippopov32@gmail.com>
2019-01-14 15:45:24 +0300

    HwAPI. HwVoltageSource. Add calibration data as array to get offset and slope by source index

--- User/HwAPI/HwAPI_VoltageSource.c
+++ User/HwAPI/HwAPI_VoltageSource.c
@@ -23,6 +23,7 @@
 #include "AD56x0.h"
 
 
+// Declare private variables
 static const float sourceRanges[] = {
     NSOURCE1_RANGE,
     NSOURCE2_RANGE,
@@ -30,15 +31,22 @@ static const float sourceRanges[] = {
     NSOURCE4_RANGE
 };
 
+static const CalibrationData_t calibrationData[] = {
+    { NSOURCE1_CALIBRATION_SLOPE, NSOURCE1_CALIBRATION_OFFSET },
+    { NSOURCE2_CALIBRATION_SLOPE, NSOURCE2_CALIBRATION_OFFSET },
+    { NSOURCE3_CALIBRATION_SLOPE, NSOURCE3_CALIBRATION_OFFSET },
+    { NSOURCE4_CALIBRATION_SLOPE, NSOURCE4_CALIBRATION_OFFSET }
+};
+
 
 //
 HwAPI_StatusTypeDef HwAPI_VoltageSource_Set( NSource_t nSource, float value )
 {
     HwVoltageSourceQueueData_t hwVoltageSourceQueueData;
-
+    
     hwVoltageSourceQueueData.stateHwVoltageSource = HW_VOLTAGE_SOURCE_SET;
     hwVoltageSourceQueueData.nSource = nSource;
-    hwVoltageSourceQueueData.valueDAC = CalculateDAC( value, 
+    hwVoltageSourceQueueData.valueDAC = CalculateDAC( value * calibrationData[ nSource ].slope + calibrationData[ nSource ].offset, 
                                                       sourceRanges[ nSource ],
                                                       AD56X0_OPERATION_MODE_NORMAL );
 
