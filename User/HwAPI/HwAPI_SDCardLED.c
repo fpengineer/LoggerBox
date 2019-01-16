1a68df4d1976bbb2ac940b80e6482fee0bb3c687
FILIP\Филипп <filippopov32@gmail.com>
2019-01-16 15:43:02 +0300

    HwAPI. HwSDCardLED. Add files with implementation of functions LED On/Off/Flash

---
+++ User/HwAPI/HwAPI_SDCardLED.c
@@ -0,0 +1,63 @@
+/*****************************************************************************************************
+
+    Project: 
+    
+    Date: 
+    Author: Filipp Popov
+
+    Overview: HwAPI for SD card LED
+
+*******************************************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#include "TS_HwQueue.h"
+
+#include "HwAPI.h"
+
+
+//
+HwAPI_StatusTypeDef HwAPI_SDCardLED_On( void )
+{
+    HwSDCardLEDQueueData_t hwSDCardLEDQueueData;
+
+    hwSDCardLEDQueueData.stateHwSDCardLED = HW_SDCARD_LED_ON;
+
+    xQueueSend( xQueue_HwSDCardLED_Rx, &hwSDCardLEDQueueData, NULL );
+
+	return HW_API_OK;
+}
+
+
+//
+HwAPI_StatusTypeDef HwAPI_SDCardLED_Off( void )
+{
+    HwSDCardLEDQueueData_t hwSDCardLEDQueueData;
+
+    hwSDCardLEDQueueData.stateHwSDCardLED = HW_SDCARD_LED_OFF;
+
+    xQueueSend( xQueue_HwSDCardLED_Rx, &hwSDCardLEDQueueData, NULL );
+
+	return HW_API_OK;
+}
+
+
+//
+HwAPI_StatusTypeDef HwAPI_SDCardLED_Flash( uint16_t delay_ms )
+{
+    HwSDCardLEDQueueData_t hwSDCardLEDQueueData;
+
+    hwSDCardLEDQueueData.stateHwSDCardLED = HW_SDCARD_LED_FLASH;
+    hwSDCardLEDQueueData.delay_ms = delay_ms;
+    
+    xQueueSend( xQueue_HwSDCardLED_Rx, &hwSDCardLEDQueueData, NULL );
+
+	return HW_API_OK;
+}
+/* End of file */
---
+++ User/HwAPI/HwAPI_SDCardLED.h
@@ -0,0 +1,43 @@
+/*****************************************************************************************************
+
+    HwAPI SD card LED
+
+
+
+*******************************************************************************************************/
+#ifndef _HWAPI_SDCARD_LED_H_
+#define _HWAPI_SDCARD_LED_H_
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdio.h>
+
+#include "HwAPI Def.h"
+
+
+// SD card LED port/pin defines
+#define SDCARD_LED_PORT   GPIOC
+#define SDCARD_LED_PIN    GPIO_Pin_2
+
+// SD card LED On/Off macro
+#define SDCardLED_On()        GPIO_SetBits( SDCARD_LED_PORT, SDCARD_LED_PIN )
+#define SDCardLED_Off()       GPIO_ResetBits( SDCARD_LED_PORT, SDCARD_LED_PIN )
+#define SDCardLED_Toggle()    GPIO_ToggleBits( SDCARD_LED_PORT, SDCARD_LED_PIN )
+
+#define SDCARD_LED_FLASH_SLOW		500 // delay in milliseconds
+#define SDCARD_LED_FLASH_FAST		200 // delay in milliseconds
+
+
+/* Exported types ------------------------------------------------------------*/
+
+
+
+/* Exported functions --------------------------------------------------------*/
+HwAPI_StatusTypeDef HwAPI_SDCardLED_On( void );
+HwAPI_StatusTypeDef HwAPI_SDCardLED_Off( void );
+HwAPI_StatusTypeDef HwAPI_SDCardLED_Flash( uint16_t delay_ms );
+
+
+
+#endif /* _HWAPI_SDCARD_LED_H_*/
+/* End of file */
