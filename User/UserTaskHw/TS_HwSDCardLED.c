beb421056ca5d47294a9d233a0d77fdf6457f570
FILIP\Филипп <filippopov32@gmail.com>
2019-01-16 15:35:59 +0300

    HwAPI. HwSDCardLED. Add task

---
+++ User/UserTaskHw/TS_HwSDCardLED.c
@@ -0,0 +1,109 @@
+/*****************************************************************************************************
+
+    Project: 
+    
+    Date: 
+    Author: Filipp Popov
+
+    Overview: Task - Hw SD card LED
+
+*******************************************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+#include "stdint.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+/* Include core modules */
+#include "stm32f4xx.h"
+/* Include my libraries here */
+#include "defines.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_gpio.h"
+
+#include "HwAPI.h"
+#include "TS_HwQueue.h"
+#include "TS_task.h"
+
+// Declare private functions
+static void InitSDCardLEDHardware( void );
+
+// Declare private variables
+
+
+void vTask_HwSDCardLED( void *pvParameters )
+{
+    HwSDCardLEDQueueData_t hwSDCardLEDQueueData;
+    TickType_t timeout = 0;
+    
+    hwSDCardLEDQueueData.stateHwSDCardLED = HW_SDCARD_LED_INIT;            
+    xQueueSend( xQueue_HwSDCardLED_Rx, &hwSDCardLEDQueueData, NULL ); 
+	while ( 1 )
+	{
+        xQueueReceive( xQueue_HwSDCardLED_Rx, &hwSDCardLEDQueueData, timeout );
+        
+        switch ( hwSDCardLEDQueueData.stateHwSDCardLED )
+        {
+            case HW_SDCARD_LED_INIT:
+            {
+				InitSDCardLEDHardware();
+				SDCardLED_Off();
+				break;
+            }
+
+            case HW_SDCARD_LED_ON:
+            {
+                SDCardLED_On();
+                timeout = portMAX_DELAY;
+                break;
+            }
+            
+            case HW_SDCARD_LED_OFF:
+            {
+				SDCardLED_Off();
+                timeout = portMAX_DELAY;
+                break;
+            }
+            
+            case HW_SDCARD_LED_FLASH:
+            {
+				SDCardLED_Toggle();
+                timeout = hwSDCardLEDQueueData.delay_ms / portTICK_PERIOD_MS;
+                break;
+            }                
+            case HW_SDCARD_LED_IDLE:
+            {
+                break;
+            }                
+
+            default:
+                break;
+        }
+    }
+}
+
+
+//*************************************************
+//
+// Private function
+//
+// Initialize hardware
+//
+//*************************************************
+static void InitSDCardLEDHardware( void )
+{
+    GPIO_InitTypeDef GPIO_cfg;
+    
+    RCC_AHB1PeriphClockCmd( RCC_AHB1Periph_GPIOC, ENABLE );
+
+    GPIO_StructInit( &GPIO_cfg );
+    GPIO_cfg.GPIO_Pin = SDCARD_LED_PIN;
+    GPIO_cfg.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_cfg.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_cfg.GPIO_OType = GPIO_OType_PP;
+    GPIO_Init( SDCARD_LED_PORT, &GPIO_cfg );
+}
+
+/* End of file */
