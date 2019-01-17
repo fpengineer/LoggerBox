9a7a824692c8511a71842a0b94dcc8e4004e9512
FILIP\Филипп <filippopov32@gmail.com>
2019-01-17 11:31:47 +0300

    HwAPI. HwFatFs. Remove defines for fatfs library

--- User/defines.h
+++ User/defines.h
@@ -28,17 +28,6 @@
 #ifndef TM_DEFINES_H
 #define TM_DEFINES_H
 
-/* Put your global defines for all libraries here used in your project */
-/* Use SPI communication with SDCard */
-#define    FATFS_USE_SDIO                0
-
-/* Select your SPI settings */
-#define FATFS_SPI                SPI1
-#define FATFS_SPI_PINSPACK        TM_SPI_PinsPack_1
-
-/* Custom CS pin for SPI communication */	
-#define FATFS_CS_PORT		GPIOC
-#define FATFS_CS_PIN		GPIO_Pin_4
 
 //Enable Card detect pin
 //#define FATFS_USE_DETECT_PIN				1
