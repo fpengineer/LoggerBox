4af5104d4c099a16523d2c4e68ea5c7dbeeb6313
FILIP\Филипп <filippopov32@gmail.com>
2019-01-31 13:40:27 +0300

    main. Relaize reset process of the system

--- User/main.c
+++ User/main.c
@@ -1,7 +1,7 @@
 /**
- *	Keil project for FatFS for SD cards
+ *	LoggerBox project
  *
- *	Before you start, select your target, on the right of the "Load" button
+ *	
  *
  *	@author		Filipp Popov
  *	@email		filip@land.ru
@@ -24,7 +24,6 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
-#include "semphr.h"
 
 // Include application
 #include "HwAPI.h"
@@ -34,25 +33,15 @@
 
 #define	ERROR_ACTION( CODE,POS )		do{}while( 0 )
 
-TaskHandle_t xTask_HwBoot;
-    
+   
 // Plugin task
-TaskHandle_t xTask_MeasurePlanner;
-TaskHandle_t xTask_MeasureX;
+TaskHandle_t xTask_HwBoot;
+//TaskHandle_t xTask_MeasurePlanner;
+//TaskHandle_t xTask_MeasureX;
 
 // Plugin queue
 QueueHandle_t xQueue_MeasurePlanner_Rx;
-QueueHandle_t xQueue_MeasureX_Rx;
-
-
-
-
-
-/* User Global Variables */    
-
-    
-    
-    
+//QueueHandle_t xQueue_MeasureX_Rx;
 
 int main(void) {
     SystemInit();
@@ -62,48 +51,32 @@ int main(void) {
 
 //    OutputMCO();
     
-    xQueue_MeasurePlanner_Rx = xQueueCreate( 5, sizeof( int ) );
+//    xQueue_MeasurePlanner_Rx = xQueueCreate( 5, sizeof( int ) );
 //    xQueue_MeasureX_Rx = xQueueCreate( 5, sizeof( int ) );
 
+    TM_DISCO_LedOn( LED_RED );
 
-    vTaskStartScheduler();
-
-    // run hardware tasks
-    if ( HwAPI_Terminal_Run() == HW_TASK_BOOT_RUN )
-    {
-        HwAPI_Terminal_SendMessage( "HwTerminal task run\n" );
-        
-        // Strictly follow boot sequence!!!
-        if ( HwAPI_SystemTime_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_StatusLED_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_SDCardLED_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_FatFs_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_SDCardDetect_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_Relay_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_VoltageSource_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_DAQ_ADC_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_DAQ_Frequency_Run() == HW_TASK_BOOT_RUN &&
-             HwAPI_RunButton_Run() == HW_TASK_BOOT_RUN )
-        {
-    // run measure planner
-#if 1       // MeasurePlanner
-            if( pdTRUE != xTaskCreate(  vTask_MeasurePlanner,
-                                        "Task - Measure Planner",
-                                        configMINIMAL_STACK_SIZE,
-                                        NULL,
-                                        tskIDLE_PRIORITY + 1,
-                                        &xTask_MeasurePlanner )) { ERROR_ACTION(TASK_NOT_CREATE,0); }	
+#if 1  // Hardware boot
+    if( pdTRUE != xTaskCreate(  vTask_HwBoot,
+                                "Task - Hardware boot",
+                                configMINIMAL_STACK_SIZE + 5000,
+                                NULL,
+                                tskIDLE_PRIORITY + 1,
+                                &xTask_HwBoot ) ) { ERROR_ACTION(TASK_NOT_CREATE,0); }	
 #endif
+    
+#if 0  // Measure planner
+    if( pdTRUE != xTaskCreate(  vTask_HwMeasurePlanner,
+                                "Task - Measure Planner",
+                                configMINIMAL_STACK_SIZE + 5000,
+                                NULL,
+                                tskIDLE_PRIORITY + 1,
+                                &xTask_HwMeasurePlanner ) ) { ERROR_ACTION(TASK_NOT_CREATE,0); }	
+#endif
+    
+    vTaskStartScheduler();
                                 
-            HwAPI_Terminal_SendMessage( "***********   LoggerBox   UF113.887   " SYSTEM_VERSION "   ***********\n" );
-        }
-    }
-    else
-    {
-        HwAPI_Terminal_SendMessage( "HwTerminal task boot error\n" );
-    }
-
-	while ( 1 )
+    while ( 1 )
     {
 
 	}
