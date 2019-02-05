dce63849595fb5774d5316287eff655604a3f184
FILIP\Филипп <filippopov32@gmail.com>
2019-02-05 14:50:47 +0300

    MeasurePlanner. Add Run button event callback. Style changes

--- User/UserTaskPlugins/TS_MeasurePlanner.c
+++ User/UserTaskPlugins/TS_MeasurePlanner.c
@@ -34,7 +34,7 @@ typedef enum {
 
 // Declare private variables
 //static char tempString[450] = {""}; 
-static MeasureStatus_t measureStatus = MEASURE_STOP
+static MeasureStatus_t measureStatus = MEASURE_STOP;
     
 void vTask_MeasurePlanner( void *pvParameters )
 {
@@ -50,69 +50,98 @@ void vTask_MeasurePlanner( void *pvParameters )
         switch ( measurePlannerQueueData.stateMeasurePlanner )
         {
             case MEASURE_PLANNER_INIT:
+            {
+                while ( hwBootData.hwBootStatus == HW_BOOT_IN_PROGRESS ||
+                        hwBootData.hwBootStatus == HW_BOOT_IDLE )
                 {
-                    while ( hwBootData.hwBootStatus == HW_BOOT_IN_PROGRESS ||
-                            hwBootData.hwBootStatus == HW_BOOT_IDLE )
-                    {
-                        hwBootData = HwAPI_Boot_GetStatus();
-                    }
+                    hwBootData = HwAPI_Boot_GetStatus();
+                }
                     
-                    if ( hwBootData.hwBootStatus == HW_BOOT_SUCCESS )
-                    {
-                        // Start measure plugin loader
-                    }
-                    else
-                    {
-                        // Error!
-                        // Hardware boot failed
-                    }
+                if ( hwBootData.hwBootStatus == HW_BOOT_SUCCESS )
+                {
+                    // Start measure plugin loader
+                }
+                else
+                {
+                    // Error!
+                    // Hardware boot failed
                 }
                 break;
+            }
                 
             case MEASURE_PLANNER_GET_TYPE:
-                {
-                    
-                }
+            {
+                  
                 break;
+            }
                 
             case MEASURE_PLANNER_RUN_MEASURE_TASK:
-                {
+            {
                     
-                }
                 break;
+            }
                 
             case MEASURE_PLANNER_STOP_MEASURE_TASK:
-                {
+            {
                     
-                }
                 break;
+            }
                 
             case MEASURE_PLANNER_IDLE:
-                {
+            {
                     
-                }
                 break;
+            }
                 
             default:
                 break;
-                
         }
     }
 }
 
 
+// SD card event callback
 void SDCardDetect_Event( SDCardDetectStatus_t sdCardDetectStatus )
 {
     switch ( sdCardDetectStatus )
     {
         case SD_CARD_INSERT:
         {    
-            // Run sd card insert handler
+            // Get configuration info
+            // Load measure plugin
             break;
         }
     
         case SD_CARD_REMOVE:
         {
+            // Stop measure if it is already run
+            // Unload measure plugin
+            // Set system to defaul state
+            break;
+    	}
+        
+        default:
+            break;
+    }
+    
+}
+
+
+// Run button event callback
+void RunButton_Event( RunButtonStatus_t runButtonStatus )
+{
+    switch ( runButtonStatus )
+    {
+        case MEASURE_STOP:
+        {    
+            // Send run command to measure plugin
+            break;
+        }
+    
+        case MEASURE_RUN:
+        {
+            // Send stop command to measure plugin
+            // Set system to default state
             break;
     	}
         
