7222ccdaadc0cd33bb89f2dec55c8a3227152490
FILIP\Филипп <filippopov32@gmail.com>
2019-02-20 15:13:10 +0300

    MeasurePlugins. Move plugins list defines and declarations to a separate file 'PluginsList.h'

--- User/MeasurePlugins/PluginsList.h
+++ User/MeasurePlugins/PluginsList.h
@@ -1,27 +1,25 @@
 /*****************************************************************************************************
 
-    Plugins defines
+    Plugins list
 
 
 
 *******************************************************************************************************/
-#ifndef _PLUGINS_DEF_H_
-#define _PLUGINS_DEF_H_
+#ifndef _PLUGINS_LIST_H_
+#define _PLUGINS_LIST_H_
 
 #include <stdint.h>
 
+#include "plgPlugin1_V1_0\plgPlugin1_V1_0.h"
+#include "plgPlugin2_V1_0\plgPlugin2_V1_0.h"
+#include "plgPlugin3_V1_0\plgPlugin3_V1_0.h"
+
 
 /* Exported types ------------------------------------------------------------*/
-typedef enum {
-    CMD_PLUGIN_RUN,
-    CMD_PLUGIN_STOP,
-    CMD_PLUGIN_TACT,
-    CMD_PLUGIN_ERROR,
-    CMD_PLUGIN_IDLE
-}PluginCommand_t;
 
 /* Exported functions --------------------------------------------------------*/
-
+int32_t GetSizeof_pluginsList( void );
+int32_t GetSizeof_pluginsListElement( void );
 
 #endif /* _PLUGINS_DEF_H_ */
 /* End of file */
