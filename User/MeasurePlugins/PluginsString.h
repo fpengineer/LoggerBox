ce006615777c4d0d0c6a2594b9ca7c044ed9a451
FILIP\Филипп <filippopov32@gmail.com>
2019-02-25 08:50:07 +0300

    MeasurePlugins. Add plugins string header file

---
+++ User/MeasurePlugins/PluginsString.h
@@ -0,0 +1,26 @@
+/*****************************************************************************************************
+
+    Plugins list
+
+
+
+*******************************************************************************************************/
+#ifndef _PLUGINS_STRING_H_
+#define _PLUGINS_STRING_H_
+
+#include <stdint.h>
+
+#define PLUGINS_STRING_SIZE         2000
+#define MEASURE_PLAN_FILENAME_SIZE  100
+
+/* Exported types ------------------------------------------------------------*/
+
+
+/* Exported functions --------------------------------------------------------*/
+int32_t GetSizeof_pluginsString( void );
+int32_t GetSizeof_measurePlanFilename( void );
+int32_t GetSizeof_pluginsStringElement( void );
+int32_t GetSizeof_measurePlanFilenameElement( void );
+
+#endif /* _PLUGINS_STRING_H_ */
+/* End of file */
