546a9c39a1d9844519e2c7d920fe795dcfc3fe51
FILIP\Филипп <filippopov32@gmail.com>
2019-03-18 09:42:54 +0300

    Plugin - plgPlugin3_V1_0. Add calibration functionality of the ADC

--- User/MeasurePlugins/plgPlugin3_V1_0/plgPlugin3_V1_0.c
+++ User/MeasurePlugins/plgPlugin3_V1_0/plgPlugin3_V1_0.c
@@ -16,6 +16,8 @@
 #include "plgPlugin3_V1_0\plgPlugin3_V1_0.h"
 #include "PluginsString.h"
 
+char string[100] = {""};
+
 void plgPlugin3( PluginResult_t *pluginResult, PluginCommand_t pluginCommand, int32_t *tactLength_ms )
 {
     switch ( pluginCommand )
@@ -25,7 +27,7 @@ void plgPlugin3( PluginResult_t *pluginResult, PluginCommand_t pluginCommand, in
 #ifdef PLUGINS_DEBUG_INFO
             HwAPI_Terminal_SendMessage( "Plugin3 - Run\n" );
 #endif
-            *tactLength_ms = 500;
+            *tactLength_ms = 2000;
             pluginResult->error = 0;
             break;
         }
@@ -44,6 +46,54 @@ void plgPlugin3( PluginResult_t *pluginResult, PluginCommand_t pluginCommand, in
 #ifdef PLUGINS_DEBUG_INFO
             HwAPI_Terminal_SendMessage( "Plugin3 - Tact\n" );
 #endif
+            float value = 0.0f;
+
+
+            HwAPI_VoltageSource_Set( NSOURCE_1, 12.0f );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_1 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC1_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_1 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_2 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC2_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_2 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_3 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC3_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_3 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_4 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC4_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_4 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_5 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC5_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_5 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_6 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC6_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_6 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_7 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC7_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_7 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_DAQ_ADC_SelectInput( ADC_CHANNEL_1, ADC_SOURCE_8 );
+            HwAPI_DAQ_ADC_GetAveraged( &value, SRC8_RANGE_V, 512 );
+            snprintf( string, sizeof(string), "ADC_SOURCE_8 = %.3f\n", value );        
+            HwAPI_Terminal_SendMessage( string );
+
+            HwAPI_Terminal_SendMessage( "\n----------------------------\n" );
+
+
             pluginResult->error = 0;
             break;
         }
